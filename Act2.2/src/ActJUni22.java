988d22929c7b89b430b427cae1b36ac3adcc5d44
davidhmhernandez <david.hmhernandez@gmail.com>
2014-09-19 11:19:33 +0200

    Version 2

---
+++ Act2.2/src/ActJUni22.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ActJUni22 {
+
+	@Test
+	public void existeFichero() {
+		String fichero = "prueba";
+		File f = new File(fichero);
+		try {
+			f.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		assertEquals(true, Main.existeFichero(fichero));
+		assertEquals(false, Main.existeFichero("Davidddd"));
+		f.delete();
+	}
+
+	@Test
+	public void datosFichero() {
+		String fichero2 = "prueba2";
+		File f2 = new File(fichero2);
+
+		try {
+			f2.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String[] datos = Main.datosFicheros(fichero2);
+		assertEquals(6, datos.length);
+		assertEquals(f2.getName(), datos[0]);
+		assertEquals(f2.getAbsolutePath(), datos[1]);
+		
+		assertEquals("True", datos[2]);
+		assertEquals("True", datos[3]);
+		int tamano = (int) f2.length();
+		String tamano2 = Integer.toString(tamano);
+		assertEquals(tamano2, datos[4]);
+		assertEquals(f2.getParent(), datos[5]);
+
+		f2.delete();
+
+	}
+	@Test
+	public void datosFichero2() {
+		String[] datos = Main.datosFicheros("supercalifragilistico");
+
+		assertEquals(6, datos.length);
+		assertEquals("supercalifragilistico", datos[0]);
+		assertEquals("", datos[1]);
+		assertEquals("False", datos[2]);
+		assertEquals("False", datos[3]);
+		assertEquals(0, datos[4]);
+		assertEquals("", datos[5]);
+	}
+
+}
--- Act2.2/src/Main.java
+++ Act2.2/src/Main.java
@@ -11,21 +11,39 @@ public class Main {
 	public static void main(String[] args) {
 		introducirRuta();
 		existeFichero(sTexto);
-		datosFicheros(sTexto);
+		//datosFicheros(sTexto);
+		for(int i=0;i<6;i++){
+			System.out.println(datosFicheros(sTexto).toString());
+		}
 			
 
 	}
 
-	private static String[] datosFicheros(String sTexto2) {
+	public static String[] datosFicheros(String sTexto2) {
+		crearFichero(sTexto2);
 		String array[] = new String[6];
+		array[0]=f.getName();
+		array[1]=f.getAbsolutePath();
+		if(f.canRead()){
+			array[2]="True";
+		}else array[2]="False";
 		
-		return null;
+		if(f.canWrite()){
+			array[3]="True";
+		}else array[3]="False";
+		
+		int tamano=(int) f.length();		
+		array[4]=Integer.toString(tamano);
+		
+		array[5]=f.getParent();
+
+		return array;
 		
 		
 	}
 
-	private static boolean existeFichero(String fichero) {
-		f = new File(fichero);
+	public static boolean existeFichero(String fichero) {
+		crearFichero(fichero);
 		if (f.exists()) {
 			System.out.println("Existe");
 			return true;
@@ -34,7 +52,11 @@ public class Main {
 			return false;
 	}
 
-	private static String introducirRuta() {
+	public static void crearFichero(String fichero) {
+		f = new File(fichero);
+	}
+
+	public static String introducirRuta() {
 		System.out.println("Indique la Ruta.");
 		Scanner reader = new Scanner(System.in);
 		sTexto = reader.next();
