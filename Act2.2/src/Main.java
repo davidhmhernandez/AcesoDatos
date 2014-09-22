9516679de423db2b2c9d8e8f8b892be0be5e48d6
David <David@David-PC>
2014-09-22 16:08:32 +0200

    Hasta el Ejercicio 5

--- Act2.2/src/Main.java
+++ Act2.2/src/Main.java
@@ -10,36 +10,79 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		introducirRuta();
+
+		System.out.println("¿Existe el Fichero?");
 		existeFichero(sTexto);
-		//datosFicheros(sTexto);
-		for(int i=0;i<6;i++){
-			System.out.println(datosFicheros(sTexto).toString());
+
+		System.out.println("");
+		System.out.println("Datos");
+		datosFicheros(sTexto);
+
+		System.out.println("");
+		System.out.println("Ruta");
+		System.out.println(ruta(absoluta(sTexto)));
+
+		System.out.println("");
+		System.out.println("Archivos");
+		imprimirContenidoDirectorio(sTexto);
+
+	}
+
+	private static String[] imprimirContenidoDirectorio(String sTexto4) {
+		crearFichero(sTexto4);
+		String[] archivos = f.list();
+
+		for (int j = 0; j < archivos.length; j++) {
+			System.out.println(archivos[j]);
 		}
-			
+
+		return archivos;
+	}
+
+	private static String ruta(boolean absoluta) {
+		if (absoluta) {
+			return f.getAbsolutePath();
+		} else
+			return f.getPath();
+
+	}
+
+	private static boolean absoluta(String sTexto3) {
+		crearFichero(sTexto3);
+		if (!f.getAbsolutePath().equalsIgnoreCase(" ")) {
+			return true;
+		} else
+			return false;
 
 	}
 
 	public static String[] datosFicheros(String sTexto2) {
 		crearFichero(sTexto2);
 		String array[] = new String[6];
-		array[0]=f.getName();
-		array[1]=f.getAbsolutePath();
-		if(f.canRead()){
-			array[2]="True";
-		}else array[2]="False";
-		
-		if(f.canWrite()){
-			array[3]="True";
-		}else array[3]="False";
-		
-		int tamano=(int) f.length();		
-		array[4]=Integer.toString(tamano);
-		
-		array[5]=f.getParent();
+		array[0] = f.getName();
+		array[1] = f.getAbsolutePath();
+		if (f.canRead()) {
+			array[2] = "True";
+		} else
+			array[2] = "False";
+
+		if (f.canWrite()) {
+			array[3] = "True";
+		} else
+			array[3] = "False";
+
+		int tamano = (int) f.length();
+		array[4] = Integer.toString(tamano);
+
+		array[5] = f.getParent();
+
+		for (int i = 0; i < 6; i++) {
+			System.out.println(array[i]);
+
+		}
 
 		return array;
-		
-		
+
 	}
 
 	public static boolean existeFichero(String fichero) {
@@ -49,7 +92,7 @@ public class Main {
 			return true;
 		} else
 			System.out.println("NO existe");
-			return false;
+		return false;
 	}
 
 	public static void crearFichero(String fichero) {
